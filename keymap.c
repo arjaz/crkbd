ef2e77eefaac7fc49a18604700aa1cfd0722b02a
Arjaz <arjaz@protonmail.com>
2022-11-11 20:42:21 +0200

    Implement a language-changing macro

--- keymap.c
+++ keymap.c
@@ -43,13 +43,29 @@
 /*     [TD_AT_CTRL] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_at, dance_at_finished, dance_at_reset), */
 /* }; */
 
+enum macros_keycodes {
+    SCROLL_LOCK_TG_1 = SAFE_RANGE,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case SCROLL_LOCK_TG_1:
+        if (record->event.pressed) {
+            SEND_STRING(SS_TAP(X_SCROLLLOCK));
+            layer_invert(1);
+        }
+        break;
+    }
+    return true;
+}
+
 /* TODO: tap-dance cyrillics @, #, * and ( */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base dvorak layer */
     [0] = LAYOUT_split_3x6_3
-    (KC_SCRL, KC_QUOT, KC_COMM,      KC_DOT,       KC_P, KC_Y, KC_F, KC_G, KC_C,         KC_R,         KC_L, KC_NO,
-     KC_TAB,  KC_A,    LCTL_T(KC_O), LALT_T(KC_E), KC_U, KC_I, KC_D, KC_H, LALT_T(KC_T), LCTL_T(KC_N), KC_S, KC_NO,
-     TG(1),   KC_SCLN, KC_Q,         KC_J,         KC_K, KC_X, KC_B, KC_M, KC_W,         KC_V,         KC_Z, OSM(MOD_RALT),
+    (KC_SCROLLLOCK,    KC_QUOT, KC_COMM,      KC_DOT,       KC_P, KC_Y, KC_F, KC_G, KC_C,         KC_R,         KC_L, KC_NO,
+     KC_TAB,           KC_A,    LCTL_T(KC_O), LALT_T(KC_E), KC_U, KC_I, KC_D, KC_H, LALT_T(KC_T), LCTL_T(KC_N), KC_S, KC_NO,
+     SCROLL_LOCK_TG_1, KC_SCLN, KC_Q,         KC_J,         KC_K, KC_X, KC_B, KC_M, KC_W,         KC_V,         KC_Z, OSM(MOD_RALT),
 
      LT(5, KC_BSPC), KC_SPC,         LGUI_T(KC_ESC),
      MO(4),          LSFT_T(KC_ENT), MO(2)
@@ -57,9 +73,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* TODO: Cyrillic layer with tap-dance for alternative letters */
     [1] = LAYOUT_split_3x6_3
-    (KC_TRNS, KC_Q, KC_W,       KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,          KC_LBRC,
-     KC_TRNS, KC_A, RALT(KC_S), KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,       RALT(KC_QUOT),
-     TG(1),   KC_Z, KC_X,       KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, RALT(KC_RBRC), KC_TRNS,
+    (KC_TRNS,          KC_Q, KC_W,       KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,          KC_LBRC,
+     KC_TRNS,          KC_A, RALT(KC_S), KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,       RALT(KC_QUOT),
+     SCROLL_LOCK_TG_1, KC_Z, KC_X,       KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, RALT(KC_RBRC), KC_TRNS,
 
      KC_TRNS, KC_TRNS, KC_TRNS,
      KC_TRNS, KC_TRNS, KC_TRNS
