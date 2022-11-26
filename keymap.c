1615e214974791533100423bcce02a25c320a613
Eugene <arjaz@protonmail.com>
2022-11-26 13:07:36 +0200

    Move layer 5 to the esc key

--- keymap.c
+++ keymap.c
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      SCROLL_LOCK_TG_1, KC_SCLN, KC_Q, KC_J, KC_K, KC_X,
      KC_B,             KC_M,    KC_W, KC_V, KC_Z, OSM(MOD_RALT),
 
-     LT(5, KC_BSPC), KC_SPC,        KC_ESC,
+     KC_BSPC,        KC_SPC,        LT(5, KC_ESC),
      LT(4, KC_TAB),  LT(2, KC_ENT), KC_NO
      ),
 
