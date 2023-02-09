b8f1ea09f3524d4a6f51a135018a5ecbe7e22b72
Arjaz <arjaz@protonmail.com>
2023-02-09 18:04:12 +0200

    Rename layer defines

--- keymap.c
+++ keymap.c
@@ -21,7 +21,7 @@ enum {
 };
 
 enum macros_keycodes {
-    SCROLL_LOCK_TG_CYR = SAFE_RANGE,
+    SCROLL_LOCK_TG_CYRILLIC = SAFE_RANGE,
 };
 
 typedef struct {
@@ -34,7 +34,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     qk_tap_dance_action_t *action;
 
     switch (keycode) {
-    case SCROLL_LOCK_TG_CYR:
+    case SCROLL_LOCK_TG_CYRILLIC:
         if (record->event.pressed) {
             SEND_STRING(SS_TAP(X_SCROLLLOCK));
             layer_invert(1);
@@ -82,30 +82,30 @@ void tap_dance_tap_hold_reset(qk_tap_dance_state_t *state, void *user_data) {
     { .fn = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}), }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_AT_CTRL]        = ACTION_TAP_DANCE_TAP_HOLD(KC_AT, KC_LCTL),
-    [TD_HASH_ALT]       = ACTION_TAP_DANCE_TAP_HOLD(KC_HASH, KC_LALT),
-    [TD_ASTR_ALT]       = ACTION_TAP_DANCE_TAP_HOLD(KC_ASTR, KC_LALT),
-    [TD_LPAREN_CTRL]    = ACTION_TAP_DANCE_TAP_HOLD(KC_LPRN, KC_LCTL),
-    [TD_EXLM_GUI]       = ACTION_TAP_DANCE_TAP_HOLD(KC_EXLM, KC_LGUI),
-    [TD_DLR_SHIFT]      = ACTION_TAP_DANCE_TAP_HOLD(KC_DLR, KC_LSFT),
-    [TD_AMPR_SHIFT]     = ACTION_TAP_DANCE_TAP_HOLD(KC_AMPR, KC_LSFT),
-    [TD_RPRN_GUI]       = ACTION_TAP_DANCE_TAP_HOLD(KC_RPRN, KC_LGUI),
-    [TD_Q_GRAVE]        = ACTION_TAP_DANCE_TAP_HOLD(KC_Q, KC_GRAVE),
-    [TD_W_QUES]         = ACTION_TAP_DANCE_TAP_HOLD(KC_W, KC_QUES),
-    [TD_E_SLASH]        = ACTION_TAP_DANCE_TAP_HOLD(KC_E, KC_SLASH),
-    [TD_T_QUOT]         = ACTION_TAP_DANCE_TAP_HOLD(KC_T, KC_QUOT),
-    [TD_U_BSLS]         = ACTION_TAP_DANCE_TAP_HOLD(KC_U, KC_BSLS),
-    [TD_P_LBRC]         = ACTION_TAP_DANCE_TAP_HOLD(KC_P, KC_LBRC),
-    [TD_BSPC_C_BSPC]    = ACTION_TAP_DANCE_TAP_HOLD(KC_BSPC, LCTL(KC_BSPC))
+    [TD_AT_CTRL]     = ACTION_TAP_DANCE_TAP_HOLD(KC_AT, KC_LCTL),
+    [TD_HASH_ALT]    = ACTION_TAP_DANCE_TAP_HOLD(KC_HASH, KC_LALT),
+    [TD_ASTR_ALT]    = ACTION_TAP_DANCE_TAP_HOLD(KC_ASTR, KC_LALT),
+    [TD_LPAREN_CTRL] = ACTION_TAP_DANCE_TAP_HOLD(KC_LPRN, KC_LCTL),
+    [TD_EXLM_GUI]    = ACTION_TAP_DANCE_TAP_HOLD(KC_EXLM, KC_LGUI),
+    [TD_DLR_SHIFT]   = ACTION_TAP_DANCE_TAP_HOLD(KC_DLR, KC_LSFT),
+    [TD_AMPR_SHIFT]  = ACTION_TAP_DANCE_TAP_HOLD(KC_AMPR, KC_LSFT),
+    [TD_RPRN_GUI]    = ACTION_TAP_DANCE_TAP_HOLD(KC_RPRN, KC_LGUI),
+    [TD_Q_GRAVE]     = ACTION_TAP_DANCE_TAP_HOLD(KC_Q, KC_GRAVE),
+    [TD_W_QUES]      = ACTION_TAP_DANCE_TAP_HOLD(KC_W, KC_QUES),
+    [TD_E_SLASH]     = ACTION_TAP_DANCE_TAP_HOLD(KC_E, KC_SLASH),
+    [TD_T_QUOT]      = ACTION_TAP_DANCE_TAP_HOLD(KC_T, KC_QUOT),
+    [TD_U_BSLS]      = ACTION_TAP_DANCE_TAP_HOLD(KC_U, KC_BSLS),
+    [TD_P_LBRC]      = ACTION_TAP_DANCE_TAP_HOLD(KC_P, KC_LBRC),
+    [TD_BSPC_C_BSPC] = ACTION_TAP_DANCE_TAP_HOLD(KC_BSPC, LCTL(KC_BSPC))
 };
 
-#define DRK_LAYER 0
-#define CYR_LAYER 1
-#define NAV_LAYER 2
-#define QWE_LAYER 3
-#define SYM_LAYER 4
-#define FNK_LAYER 5
-#define STN_LAYER 6
+#define DVORAK_LAYER 0
+#define CYRILLIC_LAYER 1
+#define NAVIGATION_LAYER 2
+#define GAMING_LAYER 3
+#define SYMBOL_LAYER 4
+#define NUMPAD_LAYER 5
+#define STENO_LAYER 6
 
 const uint16_t PROGMEM combo_comm_dot[] = {KC_COMM, KC_DOT, COMBO_END};
 const uint16_t PROGMEM combo_p_y[] = {KC_P, KC_Y, COMBO_END};
@@ -114,14 +114,14 @@ const uint16_t PROGMEM combo_f_g[] = {KC_F, KC_G, COMBO_END};
 const uint16_t PROGMEM combo_c_r[] = {KC_C, KC_R, COMBO_END};
 combo_t key_combos[COMBO_COUNT] = {
 	COMBO(combo_comm_dot, KC_ESC),
-    COMBO(combo_p_y, SCROLL_LOCK_TG_CYR),
+    COMBO(combo_p_y, SCROLL_LOCK_TG_CYRILLIC),
     COMBO(combo_u_h, CAPSWRD),
-    COMBO(combo_f_g, TG(STN_LAYER)),
-    COMBO(combo_c_r, TG(QWE_LAYER))
+    COMBO(combo_f_g, TG(STENO_LAYER)),
+    COMBO(combo_c_r, TG(GAMING_LAYER))
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [DRK_LAYER] = LAYOUT_split_3x6_3
+    [DVORAK_LAYER] = LAYOUT_split_3x6_3
     (KC_NO, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y,
      KC_F,  KC_G,    KC_C,    KC_R,   KC_L, KC_NO,
 
@@ -131,11 +131,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_NO, KC_SCLN, KC_Q, KC_J, KC_K, KC_X,
      KC_B,  KC_M,    KC_W, KC_V, KC_Z, KC_NO,
 
-     KC_NO,                 LT(FNK_LAYER, KC_SPC), TD(TD_BSPC_C_BSPC),
-     LT(NAV_LAYER, KC_TAB), LT(SYM_LAYER, KC_ENT), KC_NO
+     KC_NO, LT(NUMPAD_LAYER, KC_SPC), TD(TD_BSPC_C_BSPC),
+     LT(NAVIGATION_LAYER, KC_TAB), LT(SYMBOL_LAYER, KC_ENT), KC_NO
      ),
 
-    [CYR_LAYER] = LAYOUT_split_3x6_3
+    [CYRILLIC_LAYER] = LAYOUT_split_3x6_3
     (KC_NO, TD(TD_Q_GRAVE), TD(TD_W_QUES), TD(TD_E_SLASH), KC_R,          TD(TD_T_QUOT),
      KC_Y,  TD(TD_U_BSLS),  KC_I,          KC_O,           TD(TD_P_LBRC), KC_NO,
 
@@ -149,7 +149,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TRNS, KC_TRNS, KC_TRNS
      ),
 
-    [NAV_LAYER] = LAYOUT_split_3x6_3
+    [NAVIGATION_LAYER] = LAYOUT_split_3x6_3
     (KC_NO, KC_NO, KC_WH_D, KC_MS_U, KC_WH_U, KC_NO,      KC_NO, KC_VOLD, KC_UP,   KC_VOLU, KC_NO, KC_NO,
      KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_MS_BTN3, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO,
      KC_NO, KC_NO, KC_NO,   KC_HOME, KC_END,  KC_NO,      KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
@@ -158,18 +158,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_NO, KC_NO,      KC_NO
      ),
 
-    [QWE_LAYER] = LAYOUT_split_3x6_3
+    /* TODO: remove stuff from the outer columns */
+    [GAMING_LAYER] = LAYOUT_split_3x6_3
     (KC_LSFT,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC,
      KC_TAB,   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
      KC_LCTRL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_RBRC, KC_TRNS,
-     KC_LALT,  KC_SPC,  LT(FNK_LAYER, KC_BSPC),
+     KC_LALT,  KC_SPC,  LT(NUMPAD_LAYER, KC_BSPC),
      KC_TRNS,  KC_TRNS, KC_TRNS
      ),
 
     /* TODO: organize those better,
-       right now the symbols just follow the numbers
+       right now the SYMBOLbols just follow the numbers
     */
-    [SYM_LAYER] = LAYOUT_split_3x6_3
+    [SYMBOL_LAYER] = LAYOUT_split_3x6_3
     (KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5,
      KC_6,  KC_7, KC_8, KC_9, KC_0, KC_NO,
 
@@ -183,21 +184,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TRNS, KC_TRNS, KC_TRNS
      ),
 
-    [FNK_LAYER] = LAYOUT_split_3x6_3
-    (KC_NO, KC_NO, KC_7,  KC_8,  KC_9,   KC_NO,
-     KC_NO, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO,
-
-     KC_NO, KC_0,  KC_4,  KC_5,  KC_6,   KC_NO,
-     KC_NO, KC_F4, KC_F5, KC_F6, KC_F11, KC_NO,
-
-     KC_NO, KC_NO, KC_1,  KC_2,  KC_3,   KC_NO,
-     KC_NO, KC_F1, KC_F2, KC_F3, KC_F12, KC_NO,
-
-     KC_NO, KC_NO, KC_NO,
-     KC_NO, KC_NO, KC_NO
+    [NUMPAD_LAYER] = LAYOUT_split_3x6_3
+    (KC_NO, KC_NO, KC_7,  KC_8,  KC_9, KC_NO, KC_NO, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO,
+     KC_NO, KC_0,  KC_4,  KC_5,  KC_6, KC_NO, KC_NO, KC_F4, KC_F5, KC_F6, KC_F11, KC_NO,
+     KC_NO, KC_NO, KC_1,  KC_2,  KC_3, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F12, KC_NO,
+     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
      ),
 
-    [STN_LAYER] = LAYOUT_split_3x6_3
+    [STENO_LAYER] = LAYOUT_split_3x6_3
     (KC_NO,  STN_N1, STN_N2, STN_N3, STN_N4, STN_N5,
      STN_N6, STN_N7, STN_N8, STN_N9, STN_N9, STN_N9,
 
