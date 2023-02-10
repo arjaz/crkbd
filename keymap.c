6c93846d0bd62e7e385aea5c42c3dd8b9d33983e
Arjaz <arjaz@protonmail.com>
2023-02-10 18:24:53 +0200

    Move the numpad layer toggle back

--- keymap.c
+++ keymap.c
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_NO, KC_SCLN, KC_Q, KC_J, KC_K, KC_X,
      KC_B,  KC_M,    KC_W, KC_V, KC_Z, KC_NO,
 
-     KC_NO, LT(NUMPAD_LAYER, KC_SPC), TD(TD_BSPC_C_BSPC),
+     KC_NO, KC_SPC, LT(NUMPAD_LAYER, KC_BSPC),
      LT(NAVIGATION_LAYER, KC_TAB), LT(SYMBOL_LAYER, KC_ENT), KC_NO
      ),
 
@@ -163,12 +163,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     (KC_LSFT,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC,
      KC_TAB,   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
      KC_LCTRL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_RBRC, KC_TRNS,
-     KC_LALT,  KC_SPC,  LT(NUMPAD_LAYER, KC_BSPC),
+     KC_LALT,  KC_TRNS, KC_TRNS,
      KC_TRNS,  KC_TRNS, KC_TRNS
      ),
 
     /* TODO: organize those better,
-       right now the SYMBOLbols just follow the numbers
+       right now the symbols just follow the numbers
     */
     [SYMBOL_LAYER] = LAYOUT_split_3x6_3
     (KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5,
