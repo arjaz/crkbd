311c5b52e31f0bf214634780ca6ff8e05a4a421a
Eugene Rossokha <arjaz@tuta.io>
2023-05-10 21:38:04 +0300

    Adjust the number layer

--- keymap.c
+++ keymap.c
@@ -284,10 +284,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [NUMBER_LAYER] = LAYOUT_split_3x6_3
     (KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 
-     KC_NO, KC_6, KC_7, KC_8, KC_9, KC_NO, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO,
-     KC_NO, KC_2, KC_3, KC_4, KC_5, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,  KC_NO,
+     KC_NO, KC_8, KC_7, KC_6, KC_5, KC_NO, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO,
+     KC_NO, KC_4, KC_3, KC_2, KC_1, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,  KC_NO,
 
-     KC_TRNS, KC_1, KC_0, KC_F11, KC_F12, KC_TRNS
+     KC_TRNS, KC_0, KC_9, KC_F11, KC_F12, KC_TRNS
      ),
 
     [STENO_LAYER] = LAYOUT_split_3x6_3
