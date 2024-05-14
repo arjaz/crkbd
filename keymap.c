1860f4d6b9f5c8375a173a67b6a1f6f0646e61e7
Eugene Rossokha <hsugeneyos@gmail.com>
2024-05-14 22:13:24 +0300

    Move 0 to the index finger

--- keymap.c
+++ keymap.c
@@ -263,8 +263,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [NUMBER_LAYER] = LAYOUT_split_3x5_3
     (KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_F9, KC_F10, KC_F11, KC_F12,
      KC_8,  KC_7,  KC_6,  KC_5,  KC_NO, KC_TRNS, AR_F5, KC_F6,  KC_F7,  KC_F8,
-     KC_4,  KC_3,  KC_2,  KC_1,  KC_NO, KC_TRNS, KC_F1, KC_F2,  KC_F3,  KC_F4,
+     KC_4,  KC_3,  KC_2,  KC_1,  KC_0,  KC_TRNS, KC_F1, KC_F2,  KC_F3,  KC_F4,
 
-     KC_TRNS, KC_9, KC_0, KC_TRNS, KC_TRNS, KC_TRNS
+     KC_TRNS, KC_9, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
      ),
 };
