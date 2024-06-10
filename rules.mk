ff80d6fa69e822e8fac33ee30438931e92f5febe
Eugene Rossokha <hsugeneyos@gmail.com>
2024-06-10 09:58:38 +0300

    Change the debounce type to asym eager defer pk

--- rules.mk
+++ rules.mk
@@ -6,6 +6,7 @@ COMBO_ENABLE        = yes
 RGBLIGHT_ENABLE     = no
 BOOTLOADER          = caterina
 KEY_OVERRIDE_ENABLE = yes
+DEBOUNCE_TYPE       = asym_eager_defer_pk
 
 SRC += features/tap_hold_dance.c
 SRC += features/achordion.c
